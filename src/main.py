8145dba2400fe3f18252251a689db06fe3884a75
volodymyr <zalorderon3331@gmail.com>
2024-07-05 02:13:19 +0300

    changed structure

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """Entrypoint for the project. Extracts data from open-meteo and loads to a MySql database."""
 from weather_data_extraction import get_weather_data
-from insert_data_rds import insert_df_to_db
-from consts import TARGET_CITIES, CITIES_DATA_TABLE_NAME, WEATHER_AGGREGATED_TABLE_NAME
+from src.insert_data_rds import insert_df_to_db
+from src.consts import TARGET_CITIES, CITIES_DATA_TABLE_NAME, WEATHER_AGGREGATED_TABLE_NAME
 
 if __name__ == '__main__':
     cities_df, weather_df = get_weather_data(TARGET_CITIES)
